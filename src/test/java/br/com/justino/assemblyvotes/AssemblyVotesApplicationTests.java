711fd6dfac7b7efc76d745832dfd76078c65c011
FeJustino <fwxjustino@gmail.com>
2023-10-07 14:28:52 -0300

    feat: init test

---
+++ src/test/java/br/com/justino/assemblyvotes/AssemblyVotesApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.justino.assemblyvotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootTest
+@ComponentScan(basePackages = "br.com.justino.assemblyvotes")
+class AssemblyVotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
